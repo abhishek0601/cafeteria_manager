c7b833fac0f761da01394cb7506ba3abd6a6d1ec
abhishek0601 <abhishek.nithya@gmail.com>
2020-05-24 10:54:52 +0530

    Modified Sign-up and Login forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :ensure_user_logged_in
+  skip_before_action :ensure_user_logged_in, :except => [:index]
 
   def index
     render "index"
